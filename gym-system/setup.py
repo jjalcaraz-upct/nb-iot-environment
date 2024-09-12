b53b8c737dbdb5c7406cd695c4e753e9e6ca2616
jjalcaraz-upct <58084200+jjalcaraz-upct@users.noreply.github.com>
2024-09-12 18:53:35 +0200

    Add files via upload

--- gym-system/setup.py
+++ gym-system/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='gym_system',
-      version='0.0.1',
+      version='0.0.2',
       description='Gym User Env - generic system',
       author='Juan Jose Alcaraz',
       packages=['gym_system'],
       author_email='juan.alcaraz@upct.es',
       license='MIT License',
-      install_requires=['gym'],
+      install_requires=["gymnasium"],
       )
